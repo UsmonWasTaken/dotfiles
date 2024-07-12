35e8031f3e03ca57cf8114eabc64135bf07c468e
Usmon Abdurakhmanov <usmonwastaken@gmail.com>
2024-07-12 19:52:47 +0500

    add zsh config files

---
+++ zsh/core/aliases.zsh
@@ -0,0 +1,18 @@
+# Editor
+alias v="nvim"
+alias sv="sudo -E -s nvim"
+
+# File listing
+alias ls="eza --color=always --icons=always --group-directories-first --hyperlink"
+alias ll="eza --long --color=always --icons=always --group-directories-first --hyperlink"
+alias la="eza --all --long --color=always --icons=always --group-directories-first --hyperlink"
+alias lt="eza --tree --color=always --icons=always --group-directories-first --hyperlink"
+
+# Navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Minimalism
+alias c="clear"
