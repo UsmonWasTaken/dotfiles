4f0cedb94d73ef9c177d9df05285dcb902c5bef0
Usmon Abdurakhmanov <usmonwastaken@gmail.com>
2024-07-12 21:25:43 +0500

    change on zsh aliase

--- zsh/core/aliases.zsh
+++ zsh/core/aliases.zsh
@@ -3,10 +3,10 @@ alias v="nvim"
 alias sv="sudo -E -s nvim"
 
 # File listing
-alias ls="eza --color=always --icons=always --group-directories-first --hyperlink"
-alias ll="eza --long --color=always --icons=always --group-directories-first --hyperlink"
+alias ls="eza --color=always --icons=always --group-directories-first --hyperlink --git-ignore"
+alias ll="eza --long --color=always --icons=always --group-directories-first --hyperlink --git-ignore"
 alias la="eza --all --long --color=always --icons=always --group-directories-first --hyperlink"
-alias lt="eza --tree --color=always --icons=always --group-directories-first --hyperlink"
+alias lt="eza --tree --color=always --icons=always --group-directories-first --hyperlink --git-ignore"
 
 # Navigation
 alias ..="cd .."
