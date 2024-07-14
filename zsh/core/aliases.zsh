010cf3168725ed0cf9fe8be4484e68c89ba6fc5b
Usmon Abdurakhmanov <usmonwastaken@gmail.com>
2024-07-14 14:08:09 +0500

    add zoxide fot better navigation

--- zsh/core/aliases.zsh
+++ zsh/core/aliases.zsh
@@ -7,12 +7,14 @@ alias ls="eza --color=always --icons=always --group-directories-first --hyperlin
 alias ll="eza --long --color=always --icons=always --group-directories-first --hyperlink --git-ignore"
 alias la="eza --all --long --color=always --icons=always --group-directories-first --hyperlink"
 alias lt="eza --tree --color=always --icons=always --group-directories-first --hyperlink --git-ignore"
+alias lta="eza --tree --all --color=always --icons=always --group-directories-first --hyperlink --git-ignore"
 
 # Navigation
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
+alias cd="z"
+alias ..="z .."
+alias ...="z ../.."
+alias ....="z ../../.."
+alias .....="z ../../../.."
 
 # Minimalism
 alias c="clear"
